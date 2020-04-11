203ff703c6c8e9151ab76c5e26096dbbc44ccd5d
robdan89 <fuquardwork@gmail.com>
2020-04-11 12:09:03 -0500

    initial commit

---
+++ scripts/politicians/Politician.js
@@ -0,0 +1,11 @@
+<article class="politicians">
+  <section class="politician">
+    <header class="politician__name">
+      <h1>Charlene Harding</h1>
+    </header>
+    <div class="politician__info">
+      <div>Age: 45</div>
+      <div>Represents: Bethpage, Maine</div>
+    </div>
+  </section>
+</article>;
